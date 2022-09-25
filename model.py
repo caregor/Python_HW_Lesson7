f52f5a3c27add7131206e9c8f2cdf5ec5a524ea8
Caregor <caregor@gmail.com>
2022-09-25 20:19:20 +0300

    testing model. Add 1 contact to csv

--- model.py
+++ model.py
@@ -2,14 +2,14 @@ import csv
 
 
 def import_contact(person):
-    with open('employers.csv', 'a', newline='') as csvfile:
+    with open('contacts.csv', 'a', newline='') as csvfile:
         fieldnames = ['first_name', 'last_name', 'phone', 'description']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(person)
 
 
 def export_contact(key_value):
-    with open('employers.csv', newline='') as csvfile:
+    with open('contacts.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             if key_value == row.get('last_name'):
