d57d20666b4463342d4105b1789bccf0e39c8b60
Caregor <caregor@gmail.com>
2022-09-25 19:52:46 +0300

    add model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+import csv
+
+
+def import_contact(person):
+    with open('employers.csv', 'a', newline='') as csvfile:
+        fieldnames = ['first_name', 'last_name', 'phone', 'description']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(person)
+
+
+def export_contact(key_value):
+    with open('employers.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if key_value == row.get('last_name'):
+                print(row['first_name'], row['last_name'], row['phone'], row['description'])
